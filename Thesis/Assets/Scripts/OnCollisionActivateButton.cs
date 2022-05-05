ec414082c2000078a96dc76fb99483734cf11a93
Irene Checo <ireche96@gmail.com>
2022-05-05 09:29:00 +0200

    Changed Unity version
    Hands animation sync ok, voice ok, OpenVR ok
    Collider trigger not working

---
+++ Thesis/Assets/Scripts/OnCollisionActivateButton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class OnCollisionActivateButton : MonoBehaviourPunCallbacks, IPunObservable
+{
+    private string handshake_button = "Handshake Button";
+    private PhotonView photonView;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+    }
+
+    private void onTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision triggered");
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+        GameObject.Find(handshake_button).GetComponent<Button>().interactable = true;
+       
+    }
+
+    private void onTriggerExit(Collider other)
+    {
+
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+        GameObject.Find(handshake_button).GetComponent<Button>().interactable = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        //sync interface
+    }
+}
